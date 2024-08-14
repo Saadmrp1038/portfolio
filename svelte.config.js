64883a9c76018d8334589f6eb0fa468ad19a493d
Saadmrp1038 <saadmrp222@gmail.com>
2024-08-15 02:20:00 +0600

    deploy stuff added

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH
+    }
   },
   preprocess: vitePreprocess()
 };

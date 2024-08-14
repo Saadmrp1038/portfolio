9d71593e9cc0004318c6c34fec3bd7f85717f030
Saadmrp1038 <saadmrp222@gmail.com>
2024-08-15 02:37:54 +0600

    repo name set in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: process.argv.includes('dev') ? '' : '/portfolio'
     }
   },
   preprocess: vitePreprocess()
